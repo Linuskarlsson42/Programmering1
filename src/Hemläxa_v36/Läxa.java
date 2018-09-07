0519f146449fe6d4d35d4991254cf3dbd73b4ba3
Linuskarlsson42 <linus.che@hotmail.com>
2018-09-07 15:00:49 +0200

    uppgift 9/7
    andra commit

--- src/Hemläxa_v36/Läxa.java
+++ src/Hemläxa_v36/Läxa.java
@@ -1,5 +1,16 @@
 package Hemläxa_v36;
 
+import java.util.Scanner;
+
 public class Läxa {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		String namn = input.nextLine() 
+		
+	}
+		
 
 }
