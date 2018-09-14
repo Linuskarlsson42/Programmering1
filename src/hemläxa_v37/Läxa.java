6c986c8b8ad7eacbb0998890abeffcd47a8bdbb2
Linuskarlsson42 <linus.che@hotmail.com>
2018-09-14 14:38:44 +0200

    Commit ett

---
+++ src/hemläxa_v37/Läxa.java
@@ -0,0 +1,35 @@
+package hemläxa_v37;
+
+import java.util.Scanner;
+
+public class Läxa {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Skriv in 10 olika heltal");
+		
+		int tal1 = input.nextInt();
+		int tal2 = input.nextInt();
+		int tal3 = input.nextInt();
+		int tal4 = input.nextInt();
+		int tal5 = input.nextInt();
+		int tal6 = input.nextInt();
+		int tal7 = input.nextInt();
+		int tal8 = input.nextInt();
+		int tal9 = input.nextInt();
+		int tal10 = input.nextInt();
+		
+		int max = 99999999;
+		int min = 99999999;
+		
+		if(tal1 > max);{
+			System.out.println();
+		}
+		
+	
+		
+	}
+	
+}
