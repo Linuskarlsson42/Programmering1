585730014f3567c014cc2c155fc992069bfb8adc
Linuskarlsson42 <linus.che@hotmail.com>
2018-09-19 16:09:31 +0200

    commit 1
    Uppgift 1 och 2 klara

---
+++ src/lektionsuppgifter/uppgifter_v38.java
@@ -0,0 +1,22 @@
+package lektionsuppgifter;
+
+import java.util.Scanner;
+
+public class uppgifter_v38 {
+	public static void main(String[] args) {
+		
+		int summa = 0;
+		for (int i = 0; i < 100; i++) {
+			summa += i;
+			System.out.print(summa + " ");
+		}
+			System.out.println();
+		for (int i = 25; i < 51; i++) {
+			System.out.print(i + " ");
+			
+		}	
+			
+		
+	}
+
+}
