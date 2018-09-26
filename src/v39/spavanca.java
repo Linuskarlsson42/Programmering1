fb23e912796d234ba0e1c7cf4bf2e1825e5f17e5
Linuskarlsson42 <linus.che@hotmail.com>
2018-09-26 16:43:32 +0200

    commit 2
    Klart

--- src/v39/spavanca.java
+++ src/v39/spavanca.java
@@ -5,12 +5,32 @@ import java.util.Scanner;
 public class spavanca {
 
 	public static void main(String[] args) {
+		
 		Scanner input = new Scanner(System.in);
 		
 		int H = input.nextInt();
 		int M = input.nextInt();
 		
-		if(H % )
+		M = (M - 45);
+		
+		if(H == 0) {
+			H = 23;
+			M = M + 60;
+			System.out.println(H + " " + M);
+			
+		}
+		
+		else if(M < 0) {
+			H--;
+			M = M + 60;
+			System.out.println(H + " " + M);
+		}
+		else if(M > 0){
+			
+			System.out.println(H + " " + M);
+		}
+		
+		
 		
 		
 		
