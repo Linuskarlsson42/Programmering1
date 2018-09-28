c370b56c33c03254799395e25cfc82b845495d96
Linuskarlsson42 <linus.che@hotmail.com>
2018-09-28 15:05:19 +0200

    commit 3

---
+++ src/lektionsuppgifter2/uppgift2.java
@@ -0,0 +1,28 @@
+package lektionsuppgifter2;
+
+import java.util.Scanner;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+public class uppgift2 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int tal = input.nextInt();
+		int summa = 0;
+		
+		while(tal != 0) {
+			int ental = tal % 10;
+			summa = summa + ental % 10;
+			tal = tal - ental % 10;
+			tal = tal / 10;
+		}
+		System.out.println(summa);
+		
+		
+		
+		
+	}
+
+}
