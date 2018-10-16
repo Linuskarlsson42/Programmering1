404da0d33089f437ea9c0bfafc4afba484b2285d
Linuskarlsson42 <linus.che@hotmail.com>
2018-10-16 13:39:27 +0200

    commit 2

--- src/V41_hemläxa/v41_läxa1.java
+++ src/V41_hemläxa/v41_läxa1.java
@@ -3,14 +3,23 @@ package V41_hemläxa;
 import java.util.Scanner;
 
 public class v41_läxa1 {
+	private static String str;
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		double radius = input.nextDouble();
+		double radius = input.nextDouble();input.nextLine();
 		System.out.println(volume(radius));
+		System.out.println(reverse(str));
+		
 	}//end
 	public static double volume(double radius) {
 		radius = (3*3.14*radius*radius*radius);
 		return radius;
-	}
-	
+	}//end
+	public static String reverse(String str) {
+		int l = str.length();
+		for (int i = 0; i < l; i--) {
+			System.out.println(str.charAt(i));
+		}
+		return str;
+	}//end
 }//end
