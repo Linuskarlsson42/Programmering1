7f89c43f85ff1010d12d222e430101af95eafbc3
Linuskarlsson42 <linus.che@hotmail.com>
2018-11-21 17:47:16 +0100

    commit 2
    orkar helt enkelt inte göra denna uppgift men försökte mitt bästa.

--- src/BonusUppgifter/Everywhere.java
+++ src/BonusUppgifter/Everywhere.java
@@ -7,12 +7,32 @@ public class Everywhere {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int cases = sc.nextInt();
+		
 		for (int i = 0; i < cases; i++) {
+			
 			int NumberOfCities = sc.nextInt();
+			
 			String[] cities = new String[NumberOfCities];
+			boolean[] test = new boolean[cities.length];
+			
 			for (int j = 0; j < NumberOfCities; j++) {
 				cities[j] = sc.next();
+				test[j] = false;
+			}
+			for (int j = 0; j < cities.length; j++) {
+				if(test[j] != true) {
+					for (int j2= 0; j2 < test.length; j2++) {
+						if(j != j2) {
+							if(cities[j].equals(cities[j2])) {
+								test[j2] = true;
+							}
+						}
+					}
+					test[j] = true;
+				}
 			}
 		}
+		
 	}
+	
 }
