0294c8ce5934d73135761888ba65e3c58148e0b3
Linuskarlsson42 <linus.che@hotmail.com>
2019-08-26 10:51:28 +0200

    uppgift 5 klAArrrt

---
+++ src/Repetition/person.java
@@ -0,0 +1,16 @@
+package Repetition;
+
+public class person {
+	
+	//uppgift 5
+	
+	public String namn;
+	public int age;
+	
+	public person(String namn, int age) {
+		
+		this.namn = namn;
+		this.age = age;
+		
+	}
+}
