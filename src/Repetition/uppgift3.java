f029235981d2601bc30e317d7e397a2cb971c6f8
Linuskarlsson42 <linus.che@hotmail.com>
2019-08-26 10:26:31 +0200

    uppgift 3 klart

--- src/Repetition/uppgift3.java
+++ src/Repetition/uppgift3.java
@@ -1,5 +1,23 @@
 package Repetition;
 
+import java.util.Scanner;
+
 public class uppgift3 {
 
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("skriv in en radie");
+		
+		double radie = input.nextDouble();
+		
+		double volym = (Math.pow(radie, 3) * Math.PI * 4)/3;
+		
+		System.out.println(volym + " Volym enheter");
+		
+		
+	}
+	
 }
+
