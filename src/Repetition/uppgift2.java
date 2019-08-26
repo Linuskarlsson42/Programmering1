8036ceab58e76ab272dc9ea510371babe7e70eb2
Linuskarlsson42 <linus.che@hotmail.com>
2019-08-26 10:17:32 +0200

    uppgift 2 klart

--- src/Repetition/uppgift2.java
+++ src/Repetition/uppgift2.java
@@ -1,5 +1,23 @@
 package Repetition;
 
+import java.util.Scanner;
+
 public class uppgift2 {
 
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("skriv in tre ord");
+		
+		String ord = input.nextLine();
+		String ordII = input.nextLine();
+		String ordIII = input.nextLine();
+		
+		System.out.println(ordIII + " " + ordII + " " + ord);
+		
+		
+	}
+	
+	
 }
