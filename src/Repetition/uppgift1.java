d55dc691eb585878b7cb5538699ebcdfe46dd205
Linuskarlsson42 <linus.che@hotmail.com>
2019-08-26 10:09:18 +0200

    uppgift 1 klar

--- src/Repetition/uppgift1.java
+++ src/Repetition/uppgift1.java
@@ -2,6 +2,17 @@ package Repetition;
 
 public class uppgift1 {
 	
-	
+	public static void main(String[] args) {
+		
+		int[] heltal = {2,4,6,10};
+		
+		int summa = 0;
+		
+		for (int i = 0; i < heltal.length; i++) {
+			
+			summa += heltal[i];
+		}
+		System.out.println(summa);
+	}
 
 }
---
+++ src/Repetition/uppgift3.java
@@ -0,0 +1,5 @@
+package Repetition;
+
+public class uppgift3 {
+
+}
