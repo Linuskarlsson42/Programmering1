d0c9c6e71a836c3cd6079e07592989dbe2c88e15
Linuskarlsson42 <linus.che@hotmail.com>
2019-09-13 13:11:21 +0200

    p.2
    ändra namn

--- src/nionde_sep_inlämningar/Box.java
+++ src/nionde_sep_inlämningar/Box.java
@@ -2,14 +2,14 @@ package nionde_sep_inlämningar;
 
 import java.util.ArrayList;
 
-public class box {
+public class Box {
 	//array list som kan ta in okänt antal av primitiva datatyper
 	
 	//public Object[] a = new Object[i] samma som nedan fast odynamisk
 	
 	public ArrayList<Object> a = new ArrayList<Object>();
 	
-	public box() {
+	public Box() {
 		
 	}
 	
