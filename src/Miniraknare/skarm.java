c3b1fb2df671b4e5c980e422315b91cd027956e7
Linuskarlsson42 <linus.che@hotmail.com>
2019-10-04 14:01:21 +0200

    första commit
    gjort en skärm och knapp uppsättning för sifror och de 4 vanligaste räkne sätten. skapat en abstract class som ska ta hand om knapparna. Längre än så har jag inte kommit men PROGRES

--- src/Miniraknare/skarm.java
+++ src/Miniraknare/skarm.java
@@ -5,10 +5,20 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+/*
+ * när jag trycker på en knapp ska den anropa tangent classen, kolla vilken knapp det var jag tryckte (1, 2,
+ * +,- etc) och köra relevant metod och skicka tillbaka informationen tillbaka till skärm och skriva ut
+ * i relevant fält.
+ */
 
 
 public class skarm extends Application implements EventHandler<ActionEvent>{
+	
 
 	public static void main(String[] args) {
 		launch();
@@ -20,10 +30,10 @@ public class skarm extends Application implements EventHandler<ActionEvent>{
 		
 		Group root = new Group();
 		
-		root.getChildren().addAll();
+		root.getChildren().addAll(Gridpane());
 		
 		Scene scene = new Scene(root, 300, 300);
-		
+			
 		primaryStage.setScene(scene);
 		
 		primaryStage.show();
@@ -31,9 +41,27 @@ public class skarm extends Application implements EventHandler<ActionEvent>{
 	}
 
 	@Override
-	public void handle(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void handle(ActionEvent event) {
+		if(event.getSource()=="1") {
+			
+		}
 		
 	}	
+	
+	public static Pane Gridpane() {
+		GridPane numpad = new GridPane();
+		
+		String [] numpadKeys = {"1", "2", "3","+",
+								"4", "5", "6","-",
+								"7", "8", "9","*",
+								"0", "c", "=","/"
+								};
+		for (int i = 0; i < numpadKeys.length; i++) {
+			Button temp = new Button(numpadKeys[i]);
+			numpad.add(temp, i % 4, (int) Math.ceil(i/4));
+			
+		}
+		return numpad;
+	}
 	
 }
---
+++ src/Miniraknare/tangent.java
@@ -0,0 +1,75 @@
+package Miniraknare;
+
+abstract class tangent {
+	abstract void numbers();
+
+}
+
+class ett extends tangent{
+
+	@Override
+	void numbers() {System.out.println("1");}
+	
+}
+
+class tva extends tangent{
+
+	@Override
+	void numbers() {System.out.println("2");}
+	
+}
+
+class tre extends tangent{
+
+	@Override
+	void numbers() {System.out.println("3");}
+	
+}
+
+class fyra extends tangent{
+
+	@Override
+	void numbers() {System.out.println("4");}
+	
+}
+
+class fem extends tangent{
+	
+	@Override
+	void numbers() {System.out.println("5");}
+	
+}
+
+class sex extends tangent{
+
+	@Override
+	void numbers() {System.out.println("6");}
+	
+}
+
+class sju extends tangent{
+
+	@Override
+	void numbers() {System.out.println("7");}
+}
+
+class atta extends tangent{
+	
+	@Override
+	void numbers() {System.out.println("8");}
+	
+}
+
+class nio extends tangent{
+
+	@Override
+	void numbers() {System.out.println("9");}
+	
+}
+
+class noll extends tangent{
+
+	@Override
+	void numbers() {System.out.println("0");}
+}
+	
